f165a076fd439b4ee1981c3673b67db38a87dff3
Tim Ryan <tryan@valcom.com>
2018-04-04 13:08:38 -0400

    Add list of animals that appeared on see 'n say.

---
+++ src/animals.ts
@@ -0,0 +1,52 @@
+const barnyard = [
+  {
+    name: 'cow',
+    speak: 'moo',
+  },
+  {
+    name: 'bird',
+    speak: 'tweet tweet',
+  },
+  {
+    name: 'pig',
+    speak: 'oink oink',
+  },
+  {
+    name: 'rooster',
+    speak: 'cock-a-doodle-doo',
+  },
+  {
+    name: 'coyote',
+    speak: 'awwooooo',
+  },
+  {
+    name: 'horse',
+    speak: 'neighhhh',
+  },
+  {
+    name: 'frog',
+    speak: 'ribbit',
+  },
+  {
+    name: 'duck',
+    speak: 'quack quack',
+  },
+  {
+    name: 'dog',
+    speak: 'woof woof',
+  },
+  {
+    name: 'sheep',
+    speak: 'baaaaah',
+  },
+  {
+    name: 'turkey',
+    speak: 'gooble gooble gooble',
+  },
+  {
+    name: 'cat',
+    speak: 'meow',
+  }
+]
+
+export = barnyard;
