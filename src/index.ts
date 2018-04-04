7220cb657725645dfa0365f161c6da45968003d6
Tim Ryan <tryan@valcom.com>
2018-04-04 13:09:09 -0400

    Randomly pull an animal when code is run.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import {Command, flags} from '@oclif/command'
 
+import * as _ from 'lodash'
+
+import * as animals from './animals'
+
 class Cliandsay extends Command {
   static description = 'describe the command here'
 
@@ -8,22 +12,13 @@ class Cliandsay extends Command {
     version: flags.version({char: 'v'}),
     // add --help flag to show CLI version
     help: flags.help({char: 'h'}),
-
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    force: flags.boolean({char: 'f'}),
   }
 
-  static args = [{name: 'file'}]
-
   async run() {
-    const {args, flags} = this.parse(Cliandsay)
+    const {flags} = this.parse(Cliandsay)
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/commands/cliandsay.ts!`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    const animal = _.sample(animals);
+    this.log(`The ${animal.name} says ${animal.speak}!`)
   }
 }
 
